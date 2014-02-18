8503dc7b91ab690ecec2dfc1b471551e9027eb55
Fabricio Quagliariello <matias.quaglia@gmail.com>
2014-02-18 12:46:30 -0300

    updated with the code suggested by @mateus007

--- jquery.browser.selector.js
+++ jquery.browser.selector.js
@@ -10,22 +10,13 @@
 
   ($.browserSelector = function() {
 
-    var userAgent = navigator.userAgent.toLowerCase(),
+    var userAgent = navigator.userAgent.toLowerCase().replace(/trident\/([0-9].+) rv:/, 'ie '),
       html = document.getElementsByTagName('html')[0],
       screens = [320, 480, 640, 768, 1024, 1280, 1440, 1680, 1920],
 
       os = /(mac|win|linux|freebsd|mobile|iphone|ipod|ipad|android|blackberry|j2me|webtv)/.exec(userAgent),
       ua = /(ie|firefox|chrome|safari|opera)(?:.*version)?(?:[ \/]+)([\w.]+)/.exec(userAgent);
 
-    /**
-     *  Fix IE 11
-     */
-    if(ua === null) {
-      ua = /(trident)(?:.*rv\:)?([\w]+)/.exec(userAgent);
-      if (ua[1] === 'trident') {
-        ua[1] = 'ie';
-      }
-    }
 
     $(html).addClass(os[1] + ' ' + ua[1]);
 
